ca0f155772129b11a2d96d6433b31525588db67b
Valentino <veaziedallen@gmail.com>
2019-10-25 09:58:44 -0600

    making progress with routes

---
+++ routing/htmlRoutes.js
@@ -0,0 +1,10 @@
+let path = require("path");
+module.exports = function(app) {
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/home.html"));
+  });
+  app.get("/survey", function(req, res) {
+    console.log("hit survey");
+    res.sendFile(path.join(__dirname, "/../public/survey.html"));
+  });
+};
